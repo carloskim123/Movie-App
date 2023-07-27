9d71b2b6ca61cfda47ecde3d804e49b77822f5cc
maninthehood <115598773+maninthehood@users.noreply.github.com>
2023-07-27 08:30:22 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// HTML elements
-('use strict');
+'use strict';
 import './style.css';
+
 // HTML elements
 const searchInput = document.getElementById('search');
 const movieList = document.getElementById('movieList');
@@ -12,30 +12,29 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
-searchInput.addEventListener('input', () => {
+searchInput.addEventListener('input', async () => {
   const searchTerm = searchInput.value;
   results_for.textContent = `Results for: ${searchTerm}`;
   if (searchTerm) {
-    setTimeout(() => {
-      getMovies(searchTerm);
-    }, 500);
+    await delay(500);
+    const movies = await getMovies(searchTerm);
+    displayMovies(movies);
   }
 });
 
 // Function to fetch movies from the API
-function getMovies(searchTerm) {
+async function getMovies(searchTerm) {
   const API_KEY = 'e4b893cb06d97093f9f664b5f4d63e15'; // Replace with your own API key
   const API_URL = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&api_key=${API_KEY}`;
 
-  fetch(API_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      // Process the movie data and display it on the page
-      displayMovies(data.results);
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-    });
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.log('Error:', error);
+    return [];
+  }
 }
 
 // Function to display movies on the page
@@ -43,20 +42,25 @@ function displayMovies(movies) {
   // Clear previous movie list
   movieList.innerHTML = '';
 
-  // Check if movie data exists
   if (movies.length > 0) {
-    let movieHTML = '';
-    movies.forEach((movie) => {
-      const releaseDate = new Date(movie.release_date).toLocaleDateString();
-      movieHTML += `
-        <div id="movie-item">
-          <h2>${movie.title}</h2>
-          <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title} Poster" loading="lazy">
-        </div>
-      `;
-    });
+    const movieHTML = movies
+      .map((movie) => {
+        const releaseDate = new Date(movie.release_date).toLocaleDateString();
+        return `
+          <div id="movie-item">
+            <h2>${movie.title}</h2>
+            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title} Poster" loading="lazy">
+          </div>
+        `;
+      })
+      .join('');
     movieList.innerHTML = movieHTML;
   } else {
     movieList.innerHTML = '<p>No movies found.</p>';
   }
+}
+
+// Utility function to delay execution
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
